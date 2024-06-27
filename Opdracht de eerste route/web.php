a5a3843290b5e3a6c0cb71af38b562d28529e7e6
mechatavius <133094248+mechatavius@users.noreply.github.com>
2024-06-27 14:04:17 +0200

    Delete Opdracht de eerste route directory

--- Opdracht de eerste route/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/planets', function () {
-    $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-    return view('planets', ['planeten' => $planeten]);
-});
-
